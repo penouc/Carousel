7385104ae06cc4095dee0db3b0a1b42abd809c1b
pen <penouc@gmail.com>
2022-07-18 09:00:19 +0800

    feat: add Carousel component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { CasouselPage } from './pages';
+
+function App() {
+  return <div className="App">
+      <CasouselPage />
+    </div>;
+}
+
+export default App;
